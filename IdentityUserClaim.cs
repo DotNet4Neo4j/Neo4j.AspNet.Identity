fc924923512e8e20e684e9bf1e9f1e20a85311fd
assimoes <asvnsimoes@gmail.com>
2014-06-01 19:04:14 +0100

    initial commit

---
+++ IdentityUserClaim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neo4j.AspNet.Identity
+{
+    public class IdentityUserClaim
+    {
+     
+        public virtual string Id { get; set; }
+     
+        public virtual string UserId { get; set; }
+       
+        public virtual string ClaimType { get; set; }
+      
+        public virtual string ClaimValue { get; set; }
+    }
+}
