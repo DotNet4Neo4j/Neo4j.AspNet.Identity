4bcfeec18433fe14c9b992f39293963106743999
Chris Skardon <cskardon@xclave.co.uk>
2015-05-11 10:19:20 +0100

    Default user manager uses stronger Password Val
    Requires Digit, Lowercase and Uppercase now

--- Neo4j.AspNet.Identity/Neo4jUserManager.cs
+++ Neo4j.AspNet.Identity/Neo4jUserManager.cs
@@ -33,10 +33,10 @@
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true
-                //                RequireDigit = true,
-                //                RequireLowercase = true,
-                //                RequireUppercase = true
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true
             };
 
             // Configure user lockout defaults
