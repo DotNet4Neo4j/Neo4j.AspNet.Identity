fbe6bed1fefe1e6aa2b971d720ff19efbc924ab0
Chris Skardon <chris@tournr.com>
2015-07-28 20:21:34 +0100

    Testing case insensitivity

---
+++ Neo4j.AspNet.Identity.Tests/IdentityUserTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Neo4j.AspNet.Identity.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class IdentityUserTests
+    {
+        [Theory]
+        [InlineData("A@A.COM", "a@a.com")]
+        [InlineData("a@A.com", "a@a.com")]
+        [InlineData("a@a.com", "a@a.com")]
+        public void IgnoresCaseForUserName(string username, string expectedUsername)
+        {
+            var user = new IdentityUser(username);
+            user.UserName.Should().Be(expectedUsername);
+        }
+    }
+}
